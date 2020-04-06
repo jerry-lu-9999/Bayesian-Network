a140f15ad2c76aaf8d964486cfa57d3a0248802c
yanyujianshen <ljh274163788@gmail.com>
2020-04-06 16:23:14 -0400

    Done

--- src/bn/base/ExactInference.java
+++ src/bn/base/ExactInference.java
@@ -2,6 +2,7 @@ package bn.base;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -27,8 +28,9 @@ public class ExactInference implements Inferencer {
     public static Distribution enumerationAsk(RandomVariable X, Assignment e, BayesianNetwork network){
         Distribution q = new bn.base.Distribution(X);
         for (Value v : X.getDomain()) {
-            e.put(X, v);
-            q.put(v, enumerateAll(network.getVariablesSortedTopologically(), e, network));
+            Assignment eCopy = e.copy();
+            eCopy.put(X, v);
+            q.put(v, enumerateAll(network.getVariablesSortedTopologically(), eCopy, network));
         }
         q.normalize();
         return q;  
@@ -39,54 +41,59 @@ public class ExactInference implements Inferencer {
             return 1.0;
         }
         RandomVariable Y = vars.get(0);
-        System.out.println(Y);
         if (e.containsKey(Y)){
-            System.out.println("contains loop:" + bn.getProbability(Y, e));
-            Assignment eCopy = e.copy();
-            return bn.getProbability(Y, e) *enumerateAll(vars.subList(1, vars.size()), eCopy, bn);
+            //System.out.println("contains loop:" + bn.getProbability(Y, e));
+            //Assignment eCopy = e.copy();
+            return bn.getProbability(Y, e) *enumerateAll(rest(vars), e, bn);
         } else{
             double ysum = 0.0;
             Assignment eCopy = e.copy();
             for (Value v : Y.getDomain()){
                 eCopy.put(Y, v);
-                System.out.println("else loop: " + bn.getProbability(Y, eCopy));
-                ysum += bn.getProbability(Y, eCopy) * enumerateAll(vars.subList(1, vars.size()),eCopy, bn);
+                //System.out.println("else loop: " + bn.getProbability(Y, eCopy));
+                ysum += bn.getProbability(Y, eCopy) * enumerateAll(rest(vars),eCopy, bn);
             }
             return ysum;
         }
     }
 
+    public static List<RandomVariable> rest(List<RandomVariable> vars){
+        List<RandomVariable> rest = new ArrayList<>();
+        for (int i = 1; i < vars.size(); i++){
+            rest.add(vars.get(i));
+        }
+        return rest;
+    }
     public static void main(String[] argv) throws IOException, ParserConfigurationException, SAXException {
-        BayesianNetwork BN;
-        String filename = argv[0];
+        BayesianNetwork BN = new bn.base.BayesianNetwork();
         /* 
         * Check the format of input file
         */
+        String filename = argv[0];
         String postfix = filename.substring(filename.length()-4,filename.length());
         if (postfix.equals(".xml")){
             XMLBIFParser xmlparser = new XMLBIFParser();
             BN = xmlparser.readNetworkFromFile(filename);
         } else if(postfix.equals(".bif")){
             BIFParser bifparser = new BIFParser(new FileInputStream(filename));
+            BN = bifparser.parseNetwork();
+        } else {
+            throw new IOException("INPUT FILE IN WRONG FORMAT");
         }
 
-        String queryVariable = argv[1];
-        BooleanDomain bdomain = new BooleanDomain();
-        RandomVariable nam = new NamedVariable(queryVariable, bdomain);
+        /*
+         * Read in query variable and evidence variable
+         * Note that the value could be non-binary.
+         */
+        RandomVariable queryVariable = BN.getVariableByName(argv[1].toString());
         Assignment ass = new bn.base.Assignment();
 
         for (int i = 2; i < argv.length; i += 2) {
-            RandomVariable ran = new NamedVariable(argv[i], bdomain);
-            if(argv[i+1].equals("true") || argv[i+1].equals("false")){
-                BooleanValue boo = BooleanValue.valueOf(argv[i + 1]);
-                ass.put(ran, boo);
-            }else{
-                Value v1 = new StringValue(argv[i+1]);
-                ass.put(ran, v1);
-            }  
+            RandomVariable ran = BN.getVariableByName(argv[i]);
+            ass.put(ran, new StringValue(argv[i+1]));
         }
-        Distribution dist = enumerationAsk(nam, ass, BN);
-        System.out.println(dist.get(BooleanValue.TRUE));
+        Distribution dist = enumerationAsk(queryVariable, ass, BN);
+        System.out.println(argv[1] + " has probability distribution as " + dist);
     }
 }
 
