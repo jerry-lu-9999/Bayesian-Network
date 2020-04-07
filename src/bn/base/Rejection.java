ef1798226a2a9e2be1b75bf23887186350dcc24b
yanyujianshen <ljh274163788@gmail.com>
2020-04-06 23:50:40 -0400

    Rejection done

--- src/bn/base/Rejection.java
+++ src/bn/base/Rejection.java
@@ -1,4 +1,8 @@
+package bn.base;
+
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -9,6 +13,8 @@ import bn.core.BayesianNetwork;
 import bn.core.Distribution;
 import bn.core.Inferencer;
 import bn.core.RandomVariable;
+import bn.core.Value;
+import bn.parser.BIFParser;
 import bn.parser.XMLBIFParser;
 
 public class Rejection implements Inferencer {
@@ -19,30 +25,85 @@ public class Rejection implements Inferencer {
         return null;
     }
     
-    public static prior-sample(BayesianNetwork bn){
-        
-    }
-    public static double rejection(RandomVariable X, Assignment e, BayesianNetwork bn, int N){
-        for(int j = 1; j <= N; j++){
-
+    public static Assignment priorSample(BayesianNetwork bn, Assignment e){
+        Random rd = new Random();
+        Assignment ass = e.copy();
+        for(RandomVariable var : bn.getVariables()){
+            double random = rd.nextDouble();
+            double sum = 0.0;
+            for(Value v : var.getDomain()) {
+                ass.put(var,v);
+                double p = bn.getProbability(var, ass);
+                sum += p;
+                if(random > sum){
+                    ass.remove(var, v);
+                }else{
+                    break;
+                }
+            }
+            
         }
+        return ass;
+    }
+
+    public static Distribution rejection(RandomVariable X, Assignment e, BayesianNetwork bn, int N){
+        Distribution d = new bn.base.Distribution(X);
+        for(Value v : X.getDomain()){
+            d.put(v, 0.0);
+        }
+        for(int j = 1; j <= N; j++){
+            Assignment ass = priorSample(bn, e);
+            if(isConsistent(ass, e)){
+                Value value = ass.get(X);
+                d.put(value, d.get(value) + 1.0);
+            }
+        }
+        d.normalize();
+        return d;
+    }
+
+    public static boolean isConsistent(Assignment a, Assignment b){
+        for(RandomVariable ran : a.keySet()){
+            if(b.containsKey(ran)){
+                if(!b.get(ran).toString().equals(a.get(ran).toString())){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
     public static void main(String[] argv) throws IOException, ParserConfigurationException, SAXException {
-        String filename = argv[0];
-        XMLBIFParser parser = new XMLBIFParser();
-        BayesianNetwork BN = parser.readNetworkFromFile(filename);
+        BayesianNetwork BN = new bn.base.BayesianNetwork();
 
-        String queryVariable = argv[1];
-        BooleanDomain bdomain = new BooleanDomain();
-        RandomVariable nam = new NamedVariable(queryVariable, bdomain);
-        Assignment ass = new bn.base.Assignment();
-        
-        for (int i = 2; i < argv.length; i += 2) {
-            RandomVariable ran = new NamedVariable(argv[i], bdomain);
-            BooleanValue boo = BooleanValue.valueOf(argv[i + 1]);
-            ass.put(ran, boo);
+        int sampleSize = Integer.parseInt(argv[0]);
+        /* 
+        * Check the format of input file
+        */
+        String filename = argv[1];
+        String postfix = filename.substring(filename.length()-4,filename.length());
+        if (postfix.equals(".xml")){
+            XMLBIFParser xmlparser = new XMLBIFParser();
+            BN = xmlparser.readNetworkFromFile(filename);
+        } else if(postfix.equals(".bif")){
+            BIFParser bifparser = new BIFParser(new FileInputStream(filename));
+            BN = bifparser.parseNetwork();
+        } else {
+            throw new IOException("INPUT FILE IN WRONG FORMAT");
         }
-        Distribution dist = enumerationAsk(nam, ass, BN);
-        System.out.println(dist.get(BooleanValue.TRUE));
+
+        /*
+         * Read in query variable and evidence variable
+         * Note that the value could be non-binary.
+         */
+        RandomVariable queryVariable = BN.getVariableByName(argv[2]);
+        Assignment ass = new bn.base.Assignment();
+
+        for (int i = 3; i < argv.length; i += 2) {
+            RandomVariable ran = BN.getVariableByName(argv[i]);
+            ass.put(ran, new StringValue(argv[i+1]));
+        }
+        System.out.println(queryVariable.getDomain());
+        Distribution dist = rejection(queryVariable, ass, BN, sampleSize);
+        System.out.println(argv[2] + " has probability distribution as " + dist);
     }
 }
